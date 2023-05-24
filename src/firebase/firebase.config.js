cdd59323b3d2325102531d3d450bb75b51f77e66
tonmoye6 <tonmoybasak20567@gmail.com>
2023-05-24 07:36:15 +0600

    firebase update

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -5,15 +5,23 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCo4b--ixEKx96BI8Eo4RsDiLQNfyN1uNI",
-  authDomain: "volunterrapp.firebaseapp.com",
-  projectId: "volunterrapp",
-  storageBucket: "volunterrapp.appspot.com",
-  messagingSenderId: "44421903783",
-  appId: "1:44421903783:web:7d3d35fd616b7e2833b282"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
+
+
+// apiKey: "AIzaSyCo4b--ixEKx96BI8Eo4RsDiLQNfyN1uNI",
+// authDomain: "volunterrapp.firebaseapp.com",
+// projectId: "volunterrapp",
+// storageBucket: "volunterrapp.appspot.com",
+// messagingSenderId: "44421903783",
+// appId: "1:44421903783:web:7d3d35fd616b7e2833b282"
